a320181bbe7f5fca7dbb9179b8465e2102825841
MartinMZen <mmelekus@users.noreply.github.com>
2023-12-27 23:19:53 -0600

    Chapter 4: Writing, debugging, and testing functions

---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,15 @@
+﻿// TimesTable(7, 20);
+// ConfigureConsole(culture: "fr-FR");
+
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
+// // Alternately, call the function in the interpolated string.
+// // WriteLine($"You must pay {CalculateTax(amount: 149, twoLettrRegrionCode: "FR"):C} in tax.");
+
+// RunCardinalToOrdinal();
+
+// RunFactorial();
+
+// RunFibImperative();
+
+RunFibFunctional();
