82deafe9d606224954ada1d782aed78dfb8040e7
MartinMZen <mmelekus@users.noreply.github.com>
2024-01-02 13:55:16 -0600

    Chapter 5: Through multiple constructors.

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -2,6 +2,34 @@
 
 public class Person : object
 {
+    #region Constant fields
+    // Constant fields: Values that are fixed at compilation.
+    public const string Species = "Homo Sapiens";
+    #endregion
+
+    #region Read-only fields
+    // Read-only fields: Values that can be set at runtime.
+    public readonly string HomePlanet = "Earth";
+    public readonly DateTime Instantiated;
+    #endregion
+
+    #region Constructors: Called when using new to instantiate a type.
+    public Person()
+    {
+        // Constructors can set default values for fields
+        // including any read-only fields like Instantiated.
+        Name = "Unknown";
+        Instantiated = DateTime.Now;
+    }
+
+    public Person(string initialName, string homePlanet)
+    {
+        Name = initialName;
+        HomePlanet = homePlanet;
+        Instantiated = DateTime.Now;
+    }
+    #endregion
+
     #region Fields: Data or state for this person.
     public string? Name; // ? means it can be null.
     public DateTimeOffset Born;
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -35,6 +35,10 @@ WriteLine(
 WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
 WriteLine($"{bob.Name} has {bob.Children.Count} children:");
 for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++) { WriteLine($"> {bob.Children[childIndex].Name}"); }
+// Constant fields are accessible via the type.
+WriteLine($"{bob.Name} is a {Person.Species}.");
+// Read-only fields are accessible via the variable.
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
 
 // Object initializer syntax
 Person alice = new()
@@ -48,3 +52,38 @@ WriteLine(
     format: "{0} was born on {1:d}.", // Short date.
     arg0: alice.Name,
     arg1: alice.Born);
+
+// Static fields
+BankAccount.InterestRate = 0.012M; // Store a shared value in static field.
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: jonesAccount.AccountName,
+    arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: gerrierAccount.AccountName,
+    arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+// Required fields
+Book book = new()
+{
+    Isbn = "978-1803237800",
+    Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+};
+WriteLine($"{book.Isbn}: {book.Title} written by {book.Author} has {book.PageCount:N0} pages.");
+
+// Constructor based initialization
+Person blankPerson = new();
+WriteLine($"{blankPerson.Name} of {blankPerson.HomePlanet} was created at {blankPerson.Instantiated:hh:mm:ss} on a {blankPerson.Instantiated:dddd}.");
+
+// Multiple constructors: default vs. parameter based
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+    arg0: gunny.Name,
+    arg1: gunny.HomePlanet,
+    arg2: gunny.Instantiated);
