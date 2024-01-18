fb4927a255e3ff9244fc6eb0c9234adcdf875b18
MartinMZen <mmelekus@users.noreply.github.com>
2024-01-18 11:00:59 -0600

    Chapter 8: Regular expressions

---
+++ Chapter08/WorkingWithRegularExpressions/Program.Regexs.cs
@@ -0,0 +1,10 @@
+using System.Text.RegularExpressions; // To use [GeneratedRegex].
+
+partial class Program
+{
+    [GeneratedRegex(DigitsOnlyText, RegexOptions.IgnoreCase)]
+    private static partial Regex DigitsOnly();
+
+    [GeneratedRegex(CommaSeparatorText, RegexOptions.IgnoreCase)]
+    private static partial Regex CommaSeparator();
+}
