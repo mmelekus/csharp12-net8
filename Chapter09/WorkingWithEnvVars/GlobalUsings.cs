6879a65f9ccfc4907f3f221ba48a530e9619e64b
MartinMZen <mmelekus@users.noreply.github.com>
2024-02-09 12:24:09 -0600

    Chapter 9: Working with Environment Variables

---
+++ Chapter09/WorkingWithEnvVars/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using static System.Console;
+global using static System.Environment;
+global using Spectre.Console;
+global using System.Collections;
---
+++ Chapter09/WorkingWithEnvVars/Program.cs
@@ -0,0 +1,19 @@
+﻿SectionTitle("Reading all environment varialbles for process");
+IDictionary vars = GetEnvironmentVariables();
+DictionaryToTable(vars);
+
+SectionTitle("Reading all environment varialbes for machine");
+IDictionary varsMaching = GetEnvironmentVariables(EnvironmentVariableTarget.Machine);
+DictionaryToTable(varsMaching);
+
+SectionTitle("Reading all environment variables for user");
+IDictionary varsUser = GetEnvironmentVariables(EnvironmentVariableTarget.User);
+DictionaryToTable(varsUser);
+
+string myComputer = "My username = %USERNAME%. My CPU is %PROCESSOR_IDENTIFIER%.";
+WriteLine(ExpandEnvironmentVariables(myComputer));
+
+string password_key = "MY_PASSWORD";
+SetEnvironmentVariable(password_key, "Pa$$w0rd");
+string? password = GetEnvironmentVariable(password_key);
+WriteLine($"{password_key}: {password}");
