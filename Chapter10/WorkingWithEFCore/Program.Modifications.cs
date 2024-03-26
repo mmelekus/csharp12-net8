274547d5106bebdc28d2c0bf44e60c69f8056c58
MartinMZen <mmelekus@users.noreply.github.com>
2024-03-26 11:44:39 -0500

    Chapter 10: Completed EntityFramework modifying data

--- Chapter10/WorkingWithEFCore/Program.Modifications.cs
+++ Chapter10/WorkingWithEFCore/Program.Modifications.cs
@@ -59,4 +59,53 @@ partial class Program
         int affected = db.SaveChanges();
         return (affected, updateProduct.ProductId);
     }
+
+    private static int DeleteProducts(string productNameStartsWith)
+    {
+        using NorthwindDb db = new();
+        IQueryable<Product>? products = db.Products?.Where(p => p.ProductName.StartsWith(productNameStartsWith));
+        if (products is null || !products.Any())
+        {
+            WriteLine("No products found to delete.");
+            return 0;
+        }
+        else
+        {
+            if (db.Products is null) { return 0; }
+            db.Products.RemoveRange(products);
+        }
+        int affected = db.SaveChanges();
+        return affected;
+    }
+
+    private static (int affected, int[]? productIds) IncreaseProductPricesBetter(string productNameStartsWith, decimal amount)
+    {
+        using NorthwindDb db = new();
+        if (db.Products is null) { return (0, null); }
+
+        // Get products whose name starts with the parameter value.
+        IQueryable<Product>? products = db.Products.Where(p => p.ProductName.StartsWith(productNameStartsWith));
+        int affected = products.ExecuteUpdate(s => s.SetProperty(
+            p => p.Cost, // Property selector lambda expression.
+            p => p.Cost + amount)); // Value to update to lambda expression.
+        int[] productIds = products.Select(p => p.ProductId).ToArray();
+        return (affected, productIds);
+    }
+
+    private static int DeleteProductsBetter(string productNameStartsWith)
+    {
+        using NorthwindDb db = new();
+        int affected = 0;
+        IQueryable<Product>? products = db.Products?.Where(p => p.ProductName.StartsWith(productNameStartsWith));
+        if (products is null || !products.Any())
+        {
+            WriteLine("No products found to delete.");
+            return 0;
+        }
+        else
+        {
+            affected = products.ExecuteDelete();
+        }
+        return affected;
+    }
 }
--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -8,15 +8,45 @@ ConfigureConsole();
 // QueryingWithLike();
 // GetRandomProduct();
 // LazyLoadingWithNoTracking();
-// var resultAdd = AddProduct(categoryId: 6, productName: "Bob's Burgers", price: 500M, stock: 72);
+
+// var resultAdd = AddProduct(categoryId: 6, productName: "Bob's Burgers", price: 540M, stock: 72);
 // if (resultAdd.affected == 1)
 // {
 //     WriteLine($"Add product successful id ID: {resultAdd.productId}.");
 // }
 // ListProducts(productIdsToHighlight: new[] { resultAdd.productId });
-var resultUpdate = IncreaseProductPrice(productNameStartsWith: "Bob", amount: 20M);
-if (resultUpdate.affected == 1)
+
+// var resultUpdate = IncreaseProductPrice(productNameStartsWith: "Bob", amount: 20M);
+// if (resultUpdate.affected == 1)
+// {
+//     WriteLine($"Increase price success for ID: {resultUpdate.productId}.");
+// }
+// ListProducts(productIdsToHighlight: new[] { resultUpdate.productId });
+
+// WriteLine("About to delete all products whose name starts with \"Bob\".");
+// Write("Press Enter to continue or any other key to exit: ");
+// if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
+// {
+//     int deleted = DeleteProducts(productNameStartsWith: "Bob");
+//     WriteLine($"{deleted} product(s) were deleted.");
+// }
+// else
+// {
+//     WriteLine("Delete was canceled.");
+// }
+
+// var resultUpdateBetter = IncreaseProductPricesBetter(productNameStartsWith: "Bob", amount: 20M);
+// if (resultUpdateBetter.affected > 0) { WriteLine("Increase product price successful."); }
+// ListProducts(productIdsToHighlight: resultUpdateBetter.productIds);
+
+WriteLine("About to delete all products whose name starts with \"Bob\".");
+Write("Press Enter to continue or any other key to exit: ");
+if (ReadKey(intercept: true).Key == ConsoleKey.Enter)
 {
-    WriteLine($"Increase price success for ID: {resultUpdate.productId}.");
+    int deleted = DeleteProductsBetter(productNameStartsWith: "Bob");
+    WriteLine($"{deleted} product(s) were deleted.");
 }
-ListProducts(productIdsToHighlight: new[] { resultUpdate.productId });
+else
+{
+    WriteLine("Delete was cancelled.");
+}
